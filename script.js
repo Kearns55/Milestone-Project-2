7ea4d508f5ef229c964480129a5db48dadcf8084
Kearns55 <rebekah120@hotmail.com>
2025-08-24 20:36:53 +0200

    Declare button constants

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame);
+
 function startGame() {
 }
 function setNextQuestion() {
