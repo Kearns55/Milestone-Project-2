577d64b9124b2bdfadf5dd12952336777a51f0a0
Kearns55 <rebekah120@hotmail.com>
2025-08-24 21:25:22 +0200

    Add shuffle function to questions

--- script.js
+++ script.js
@@ -66,259 +66,281 @@ function clearStatusClass(element) {
     element.classList.remove('wrong');
 }
 
-const questions =[
+/** Questions array */
+
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+const questions = [
     {
-        question: "What is 'dog' as gaeilge?",
-        answers: [
+        question: "What is 'dog' as Gaeilge?",
+        answers: shuffle([
             { text: "Madra", correct: true },
             { text: "Cat", correct: false },
             { text: "Capall", correct: false },
             { text: "Póca", correct: false }
-        ]
+        ])
     },
   {
     question: "What is 'cat' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Cat", correct: true },
       { text: "Madra", correct: false },
       { text: "Luch", correct: false },
       { text: "Frog", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'horse' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Capall", correct: true },
       { text: "Bó", correct: false },
       { text: "Leon", correct: false },
       { text: "Eilifint", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'cow' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Bó", correct: true },
       { text: "Muc", correct: false },
       { text: "Capall", correct: false },
       { text: "Madra", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'sheep' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Caora", correct: true },
       { text: "Coinín", correct: false },
       { text: "Gabhar", correct: false },
       { text: "Loscán", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'pig' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Muc", correct: true },
       { text: "Bó", correct: false },
       { text: "Luch", correct: false },
       { text: "Francach", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'goat' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Gabhar", correct: true },
       { text: "Caora", correct: false },
       { text: "Capall", correct: false },
       { text: "Tíogar", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'chicken' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Cearc", correct: true },
       { text: "Lacha", correct: false },
       { text: "Madra", correct: false },
       { text: "Éan", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'duck' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Lacha", correct: true },
       { text: "Cearc", correct: false },
       { text: "Rón", correct: false },
       { text: "Féileacán", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'rabbit' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Coinín", correct: true },
       { text: "Luch", correct: false },
       { text: "Caora", correct: false },
       { text: "Gabhar", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'fox' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Sionnach", correct: true },
       { text: "Fia", correct: false },
       { text: "Madra", correct: false },
       { text: "Bear", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'bear' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Béar", correct: true },
       { text: "Leon", correct: false },
       { text: "Tíogar", correct: false },
       { text: "Eilifint", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'deer' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Fia", correct: true },
       { text: "Mac Tíre", correct: false },
       { text: "Féileacán", correct: false },
       { text: "Rón", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'wolf' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Mac Tíre", correct: true },
       { text: "Madra", correct: false },
       { text: "Leon", correct: false },
       { text: "Tíogar", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'mouse' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Luch", correct: true },
       { text: "Francach", correct: false },
       { text: "Coinín", correct: false },
       { text: "Ant", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'rat' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Francach", correct: true },
       { text: "Luch", correct: false },
       { text: "Iasc", correct: false },
       { text: "Madra", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'snake' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Nathair", correct: true },
       { text: "Iasc", correct: false },
       { text: "Loscán", correct: false },
       { text: "Mac Tíre", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'fish' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Iasc", correct: true },
       { text: "Deilf", correct: false },
       { text: "Míol Mór", correct: false },
       { text: "Loscán", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'bird' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Éan", correct: true },
       { text: "Bee", correct: false },
       { text: "Ant", correct: false },
       { text: "Cat", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'frog' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Loscán", correct: true },
       { text: "Iasc", correct: false },
       { text: "Coinín", correct: false },
       { text: "Luch", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'butterfly' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Féileacán", correct: true },
       { text: "Bee", correct: false },
       { text: "Seangán", correct: false },
       { text: "Éan", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'bee' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Beacha", correct: true },
       { text: "Féileacán", correct: false },
       { text: "Ant", correct: false },
       { text: "Éan", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'ant' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Seangán", correct: true },
       { text: "Francach", correct: false },
       { text: "Luch", correct: false },
       { text: "Bee", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'lion' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Leon", correct: true },
       { text: "Tíogar", correct: false },
       { text: "Béar", correct: false },
       { text: "Mac Tíre", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'tiger' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Tíogar", correct: true },
       { text: "Leon", correct: false },
       { text: "Capall", correct: false },
       { text: "Gabhar", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'elephant' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Eilifint", correct: true },
       { text: "Míol Mór", correct: false },
       { text: "Deilf", correct: false },
       { text: "Rón", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'monkey' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Moncaí", correct: true },
       { text: "Leon", correct: false },
       { text: "Tíogar", correct: false },
       { text: "Cat", correct: false }
-    ]
+    ])
   },
   {
     question: "What is 'whale' as Gaeilge?",
-    answers: [
+    answers: shuffle([
       { text: "Míol Mór", correct: true },
+{ text: "Deilf", correct: false },
       { text: "Iasc", correct: false },
-      { text: "Rón", correct: false },
-      { text: "Deilf", correct: false }
-    ]
+      { text: "Rón", correct: false }
+    ])
   },
+  {
+    question: "What is 'dolphin' as Gaeilge?",
+    answers: shuffle([
+      { text: "Deilf", correct: true },
+      { text: "Míol Mór", correct: false },
+      { text: "Iasc", correct: false },
+{ text: "Loscán", correct: false }
+    ])
+  },
+  {
+    question: "What is 'seal' as Gaeilge?",
+    answers: shuffle([
+      { text: "Rón", correct: true },
+      { text: "Iasc", correct: false },
+      { text: "Deilf", correct: false   },
   {
 
     }
