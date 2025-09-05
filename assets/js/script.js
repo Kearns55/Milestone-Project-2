13ec1dde1043d8a11d9f0751c3f1fd588a7dbe6b
Kearns55 <rebekah120@hotmail.com>
2025-09-05 18:08:22 +0200

    Add confetti animation to endQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,6 +115,23 @@ function endQuiz() {
   controls.classList.add('hide');
   scoreElement.classList.add('hide');
   document.getElementById('final-score').innerText = score;
+
+  // Trigger confetti animation
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+    var particleCount = 50 * (timeLeft / duration);
+    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+    confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+  }, 250);
 }
 
 
